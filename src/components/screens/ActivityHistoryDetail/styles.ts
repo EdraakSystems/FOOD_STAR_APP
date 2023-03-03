1155aed615fa1b45c1f3450dbe5e1c293afa2a1f
Imtiaz Ahmed <imtiaz.ahmed@edraaksystems.com>
2023-03-03 18:32:00 +0500

    first commit

---
+++ src/components/screens/ActivityHistoryDetail/styles.ts
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+export default StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30,
+    borderRadius:30
+
+  },
+  locationTrackingContainer: {
+    height: 110,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  footer: {
+    padding: 10,
+  },
+});
---
+++ src/components/screens/Home/PopularCategories/styles.ts
@@ -0,0 +1,24 @@
+import {StyleSheet, Dimensions} from 'react-native';
+export default StyleSheet.create({
+  categoryContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  categoryItem: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: Dimensions.get('window').width / 4,
+    padding: 10,
+    borderWidth: 0.3,
+  },
+  categoryImage: {
+    height: 50,
+    width: 50,
+    borderRadius:50
+  },
+  categoryTitle: {
+    fontSize: 12,
+    marginTop: 10,
+    fontWeight: 'bold',
+  },
+});
